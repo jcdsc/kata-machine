782497ade5f6348bce308b9d400685b50bee698f
“jcdsc” <jchowdsc@outlook.com>
2024-07-10 20:29:40 -0400

    added some algos

--- src/day1/QuickSort.ts
+++ src/day1/QuickSort.ts
@@ -1,3 +1,25 @@
-export default function quick_sort(arr: number[]): void {
+function qs(arr: number[], low: number, high: number): void {
+    if (low >= high) return;
+    const pivotIdx = partition(arr, low, high);
+    qs(arr, low, pivotIdx - 1);
+    qs(arr, pivotIdx + 1, high);
+}
 
+function partition(arr: number[], low: number, high: number): number {
+    const pivot = arr[high];
+    let idx = low - 1;
+    for (let i = low; i < high; i++) {
+        if (arr[i] <= pivot) {
+            idx++;
+            [arr[i], arr[idx]] = [arr[idx], arr[i]];
+        }
+    }
+    idx++;
+    arr[high] = arr[idx];
+    arr[idx] = pivot;
+    return idx;
+}
+
+export default function quick_sort(arr: number[]): void {
+    qs(arr, 0, arr.length - 1);
 }
